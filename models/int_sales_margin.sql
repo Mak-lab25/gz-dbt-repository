c632c4c3b6132bd3008605ca6605c5758b482b28
alkaren38@gmail.com <alkaren38@gmail.com>
2024-07-23 13:40:38 +0000

    operational margin calcul

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,7 +1,14 @@
-SELECT
-    *
-    ,CAST(sales.revenue as float64) * CAST(product.purchase_price as float64) as margin
-    ,CAST(sales.quantity as float64) * CAST(product.purchase_price as float64) as purchase_cost
-FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }} AS sales
-JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} AS product
-ON sales.products_id = product.products_id
+select
+    date_date,
+    orders_id,
+    sales.products_id,
+    revenue,
+    quantity,
+    purchase_price,
+    cast(sales.revenue as float64) * cast(product.purchase_price as float64) as margin,
+    cast(sales.quantity as float64)
+    * cast(product.purchase_price as float64) as purchase_cost
+from {{ ref("stg_gz_raw_data__raw_gz_sales") }} as sales
+join
+    {{ ref("stg_gz_raw_data__raw_gz_product") }} as product
+    on sales.products_id = product.products_id
